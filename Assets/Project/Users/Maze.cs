6870bea9add21df151d8d96b79029d85a1229531
Joel McKay <jtmckay2017@gmail.com>
2021-06-11 19:51:47 -0400

    Setup map script to pull data for map

---
+++ Assets/Project/Users/Maze.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GraphQlClient.Core;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+//[System.Serializable]
+//class Response
+//{
+//    public string type;
+//    public string id;
+//    public Payload payload;
+//}
+
+[System.Serializable]
+class Payload
+{
+    public Data data;
+}
+
+[System.Serializable]
+class Data
+{
+    public MazeDef[] Mazes;
+}
+
+[System.Serializable]
+class MazeDef
+{
+    public int id;
+    public string maze_name;
+    public string difficulty;
+    public DateTime created_at;
+}
+
+public class Maze : MonoBehaviour
+{
+
+    [Header("API")]
+    public GraphApi mazeApi;
+
+    [Header("Setup")]
+    public int mazeId;
+
+    public Text mazeDisplay;
+
+
+
+    private MazeDef mazeData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetMaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public async void GetMaze()
+    {
+        //Debug.Log("test");
+        UnityWebRequest request = await mazeApi.Post("GetMazes", GraphApi.Query.Type.Query);
+        Payload payload = JsonUtility.FromJson<Payload>(HttpHandler.FormatJson(request.downloadHandler.text));
+        Debug.Log(payload.data.Mazes);
+        foreach (MazeDef mz in payload.data.Mazes)
+        {
+            if (mz.id == mazeId)
+            {
+                mazeData = mz;
+            }
+        };
+        if (mazeData != null)
+        {
+            // Do some setup
+            mazeDisplay.text = mazeData.maze_name;
+        }
+
+    }
+}
+    /**
+     {"payload":
+        {"data":
+            {"Mazes": [
+                {
+                    "id":1,
+                    "maze_name":"First Maze",
+                    "difficulty":"Easy",
+                    "created_at":"2021-06-11T22:07:22.767244+00:00"
+                }
+            ]}
+         }
+       }
+
+    **/
---
+++ Assets/Project/Users/Score.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.WebSockets;
+using GraphQlClient.Core;
+using GraphQlClient.EventCallbacks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	public GameObject loading;
+
+	[Header("API")]
+	public GraphApi scoreApi;
+
+	[Header("Query")]
+	public Text queryDisplay;
+
+	[Header("Mutation")]
+	public InputField id;
+	public InputField _name;
+    public InputField time;
+
+    public Text mutationDisplay;
+
+
+	[Header("Subscription")]
+	public Text subscriptionDisplay;
+
+	private ClientWebSocket cws;
+
+	private void OnEnable()
+	{
+		OnSubscriptionDataReceived.RegisterListener(DisplayData);
+        this.Subscribe();
+	}
+
+	private void OnDisable()
+	{
+		OnSubscriptionDataReceived.UnregisterListener(DisplayData);
+	}
+
+	public async void GetQuery()
+	{
+		loading.SetActive(true);
+		UnityWebRequest request = await scoreApi.Post("GetScores", GraphApi.Query.Type.Query);
+		loading.SetActive(false);
+		queryDisplay.text = HttpHandler.FormatJson(request.downloadHandler.text);
+	}
+
+	public async void CreateNewScore()
+	{
+		loading.SetActive(true);
+		GraphApi.Query query = scoreApi.GetQueryByName("CreateNewScore", GraphApi.Query.Type.Mutation);
+		query.SetArgs(new { objects = new { name = _name.text, time = time.text } });
+		UnityWebRequest request = await scoreApi.Post(query);
+		loading.SetActive(false);
+		mutationDisplay.text = HttpHandler.FormatJson(request.downloadHandler.text);
+	}
+
+	public async void Subscribe()
+	{
+		loading.SetActive(true);
+		cws = await scoreApi.Subscribe("SubscribeToScores", GraphApi.Query.Type.Subscription, "default");
+		loading.SetActive(false);
+	}
+
+	public void DisplayData(OnSubscriptionDataReceived subscriptionDataReceived)
+	{
+		Debug.Log("I was called");
+		subscriptionDisplay.text = HttpHandler.FormatJson(subscriptionDataReceived.data);
+	}
+
+	public void CancelSubscribe()
+	{
+		scoreApi.CancelSubscription(cws);
+	}
+}
